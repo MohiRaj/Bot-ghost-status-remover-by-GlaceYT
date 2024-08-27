e375ec932519292e10fd1234cc6ca0eba08e8404
MohiRaj <mohitkumardas2580@gmail.com>
2024-08-27 14:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Watching 252 Members","Playing MinePvP"];
 
 
 let currentIndex = 0;
